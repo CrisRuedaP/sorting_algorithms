8477e0d5fc39934d4caf2303713bf420a5dd70ca
CrisRuedaP <crisruedap@gmail.com>
2020-06-16 17:07:32 +0000

    fixed output, adding a new loop

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -8,13 +8,14 @@
  */
 void shell_sort(int *array, size_t size)
 {
-	size_t i, j, gap;
+	size_t i, j, gap = 1;
 	int temp;
 
-	gap = size / 3 + 1;
-	while (gap > 0)
+	while (gap < size / 3)
+		gap = gap * 3 + 1;
+
+	while (gap >= 1)
 	{
-		j = 0;
 		for (i = gap; i < size; i++)
 		{
 			temp = array[i];
